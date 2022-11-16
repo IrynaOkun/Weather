910c988b5fe7fd9efa20071e3548f52944d6fa42
IrynaOkun <iryna.ladybug@gmail.com>
2022-11-16 21:46:48 +0200

    more refactored with added method test

--- src/test/java/PractiseTest.java
+++ src/test/java/PractiseTest.java
@@ -1,4 +1,31 @@
-public class PractiseTest {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class PractiseTest extends BaseTest {
+
+    final static By LOGO = By.xpath("//nav//a/img"
+    );
+
+    public void clickElement(By by) {
+        driver.findElement(by).click();
+    }
+
+    @Test
+    public void testHumburger() {
+
+        clickElement(LOGO);
+
+        String actualResult = driver.getCurrentUrl();
+        String expectedResult = "https://openweathermap.org/";
+
+        assertEquals(actualResult, expectedResult);
+
+
+    }
+
 
 
 }
